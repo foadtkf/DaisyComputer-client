aa41df9377fc98e61e49d968d436dbc643901211
foadtkf <fakhrulif@gmail.com>
2022-06-10 01:54:46 +0600

    not found page updated

--- src/Pages/Shared/Notfound/Notfound.js
+++ src/Pages/Shared/Notfound/Notfound.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Wobble from 'react-reveal/Wobble';
-
+import img from './c3a11496923b170c18c947fe93d13261.gif'
 const NotFound = () => {
     return (
         <Wobble>
         <div >
             
-            <img className='w-full' src="https://shots.codepen.io/djtms/pen/Exbdgob-1280.jpg?version=1646005869" alt="" />
+            <img className='w-screen' src={img} alt="" />
         </div>
         </Wobble>
     );
